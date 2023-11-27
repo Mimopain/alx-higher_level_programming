ce013254472f50a29ed994203cccda50a31c175f
Muktar <mukhtarie3@gmail.com>
2023-11-27 12:26:20 -0800

    print string

---
+++ 0x00-python-hello_world/5-print_string.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+str = "Holberton School"
+# Using string multiplication to print the string 3 times
+# Using string slicing to print the first 9 characters
+print(f"{str * 3}\n{str[:9]}\n")
+
