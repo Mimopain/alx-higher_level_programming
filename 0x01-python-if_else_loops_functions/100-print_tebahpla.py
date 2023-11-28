f88dd5b58b0a49ffacf8a4a8f4975a096577c3a6
Muktar <mukhtarie3@gmail.com>
2023-11-28 15:09:39 -0800

    tebalpha

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for i in range(ord('z'), ord('a') - 1, -1):
     print("{}".format(chr(i).lower() if i % 2 == 0
-            else chr(i).upper()), end="")
+                                      else chr(i).upper()), end="")
