0bf27ed638b186a6b0a76261043b88e381480730
Muktar <mukhtarie3@gmail.com>
2023-11-30 07:12:19 -0800

    import modules

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+from sys import argv
+
+if __name__ == "__main__":
+    argc = len(argv)
+
+    if argc != 4:
+        print("Usage: {} <a> <operator> <b>".format(argv[0]))
+        exit(1)
+
+    a, operator, b = int(argv[1]), argv[2], int(argv[3])
+
+    if operator == '+':
+        result = add(a, b)
+    elif operator == '-':
+        result = sub(a, b)
+    elif operator == '*':
+        result = mul(a, b)
+    elif operator == '/':
+        result = div(a, b)
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+
+    print("{} {} {} = {}".format(a, operator, b, result))
---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import hidden_4
+
+if __name__ == "__main__":
+    names = dir(hidden_4)
+    sorted_names = sorted(filter(lambda x: not x.startswith("__"), names))
+    for name in sorted_names:
+        print(name)
