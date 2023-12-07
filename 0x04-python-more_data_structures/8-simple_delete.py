2abd7100b732d965bf857f5ddaf2253dcec7df7a
Muktar <mukhtarie3@gmail.com>
2023-12-06 18:57:09 -0800

    Fixed

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -2,7 +2,6 @@
 def simple_delete(a_dictionary, key=""):
     a_dictionary.pop(key, None)
     return a_dictionary
-
 def print_sorted_dictionary(a_dictionary):
     for key in sorted(a_dictionary.keys()):
         print("{}: {}".format(key, a_dictionary[key]))
